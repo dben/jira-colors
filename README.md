5950e51400820eb5b97ac3c05562f84c7bed50af
David Benson <davidbenson86@gmail.com>
2023-04-17 08:45:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# JIRA colored tabs
+# =================
+
+This is a Chrome extension that adds color to the tabs in JIRA. The colors are based on the colors of the sidebar.
---
+++ script.js
@@ -0,0 +1,37 @@
+function loadCustomCss() {
+    for (item in document.getElementsByClassName("ghx-card-color-enabled")) {
+        var color = item..getElementsByClassName('ghx-grabber')[0].style.backgroundColor;
+        if (color != undefined) {
+            item.style.backgroundColor = shade(color, 0.90);
+        }
+    }    
+}
+
+function shadeRGBColor(color, percent) {
+    var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+    return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
+}
+
+function shadeColor2(color, percent) {   
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
+
+function shade(color, percent){
+    if (color.length > 7 ) return shadeRGBColor(color,percent);
+    else return shadeColor2(color,percent);
+}
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    loadCustomCss();
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+  subtree: true,
+  childList: true
+});
